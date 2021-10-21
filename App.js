335a203796f9ac397e8958cea8ca5defad92072c
codelikemax <78492851+codelikemax@users.noreply.github.com>
2021-10-21 13:22:55 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Button title="Alert" onPress={() => alert("Hey!!")}> </Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
